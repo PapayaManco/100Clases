9bba406de3f5abeaf26b1002a012f6c0bc7806ed
PapayaManco <nicojmf@uc.cl>
2024-03-10 17:59:27 -0300

    windows version

--- Test1/copiador_evosuite.py
+++ Test1/copiador_evosuite.py
@@ -1,39 +1,18 @@
-# i want to copy the file evosuite-tests/evosuite-tests-1.2.1-SNAPSHOT.jar to all the folders in the folder projects
-
 import os
 import shutil
 
-
-def copiar_a_proyectos_inyectados():
-# get the current working directory and go to the folder projects
+def copiar_a_proyectos_inyectados(evosuite_file):
     cwd = os.getcwd()
     cwd = cwd + "/projects"
-    #print(cwd)
-
-    # get the list of all the folders in the current working directory
     folders = os.listdir(cwd)
-    #print(folders)
-
-    # copy the file evosuite-tests/evosuite-tests-1.2.1-SNAPSHOT.jar to all the folders in the folder projects
-
     for folder in folders:
         if folder != ".DS_Store":
-            shutil.copyfile("evosuite-master-1.2.1-SNAPSHOT.jar", cwd + "/" + folder + "/evosuite-master-1.2.1-SNAPSHOT.jar")
-            #print("copied to " + folder)
+            shutil.copyfile(evosuite_file, cwd + "/" + folder + "/"+evosuite_file)
 
-def copiar_a_original():
-    # get the current working directory and go to the folder projects
+def copiar_a_original(evosuite_file):
     cwd = os.getcwd()
     cwd = cwd + "/projects og"
-    #print(cwd)
-
-    # get the list of all the folders in the current working directory
     folders = os.listdir(cwd)
-    #print(folders)
-
-    # copy the file evosuite-tests/evosuite-tests-1.2.1-SNAPSHOT.jar to all the folders in the folder projects
-
     for folder in folders:
         if folder != ".DS_Store" and folder != ".hgignore":
-            shutil.copyfile("evosuite-master-1.2.1-SNAPSHOT.jar", cwd + "/" + folder + "/evosuite-master-1.2.1-SNAPSHOT.jar")
-            #print("copied to " + folder)
+            shutil.copyfile(evosuite_file, cwd + "/" + folder + "/" + evosuite_file)
---
+++ Test1/main_2.py
@@ -0,0 +1,120 @@
+from copiador_evosuite import copiar_a_proyectos_inyectados
+from extraer_info import info
+from copiador_evosuite import copiar_a_original
+import subprocess
+import os
+import random
+import shutil
+c = 0
+EVOSUITE_JAR = "evosuite-master-1.0.7-SNAPSHOT.jar"
+# Funciones de preparación
+copiar_a_proyectos_inyectados(EVOSUITE_JAR)
+copiar_a_original(EVOSUITE_JAR)
+
+projects = [row[0] for row in info]
+projects = list(set(projects))
+
+projects_cp = {}
+for project in projects:
+    properties_file_path = f'projects og/{project}/evosuite-files/evosuite.properties'
+    # Check if the properties file exists
+    if os.path.exists(properties_file_path):
+        with open(properties_file_path, 'r') as f:
+            first_line = f.readline().strip()  # Read the first line and remove leading/trailing whitespace
+            # Extract the value after the '=' character
+            value_after_equal_sign = first_line.split('=')[1].strip()
+            # Transform every : into ;
+            value_after_equal_sign = value_after_equal_sign.replace(':', ';')
+            # Add the project to the dictionary
+            projects_cp[project] = value_after_equal_sign
+
+#original
+for iteracion in range(1,2):
+    seed = str(random.randint(1000000000000, 9999999999999))
+    for fila in info:
+        proyecto = fila[0]
+        clase_path = fila[1]
+        clase = fila[2]
+        print("Generando tests a la clase: ",clase)
+        # Comando sin redirección
+        command = [
+            "java",
+            "-Xmx3G",
+            "-jar",
+            EVOSUITE_JAR,
+            "-generateMOSuite",
+            "-Dalgorithm=DynaMOSA",
+            "-Dsearch_budget=180",
+            "-Dassertion_timeout=600",
+            "-Dminimization_timeout=600",
+            "-Dconfiguration_id=EvoSuite_fixed",
+            "-Doutput_variables=TARGET_CLASS,criterion,configuration_id,Total_Goals,Total_Branches,Lines,Covered_Goals,Generations,Statements_Executed,Fitness_Evaluations,Tests_Executed,Generations,Total_Time,Size,Result_Size,Length,Result_Length,BranchCoverage,Random_Seed",
+            "-Dshow_progress=FALSE",
+            "-mem","2500",
+            "-Dplot=false",
+            "-Dclient_on_thread=false",
+            "-class", clase_path,
+            "-projectCP", projects_cp[proyecto]
+            ]
+        # Nombre del archivo de salida
+        output_file = f'Dumps/seed_{iteracion}/DumpsOriginal/' + clase + '_original.txt'
+        output_directory = f'Dumps/seed_{iteracion}/DumpsOriginal'
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        try:
+            # Ejecutar el comando y redirigir la salida al archivo
+            with open(output_file, 'w') as output:
+                subprocess.run(command, cwd="projects og/" + proyecto, stdout=output, stderr=subprocess.PIPE, check=True)
+        except subprocess.CalledProcessError as e:
+            print("Error:", e)
+    print("Original listo")
+
+
+    # Injected
+    for fila in info:
+        clase_path = fila[1]
+        clase = fila[2]
+        proyecto = fila[0]
+        print("Generando tests a la clase: ",clase)
+        # Comando sin redirección
+        command = [
+            "java",
+            "-Xmx3G",
+            "-jar",
+            EVOSUITE_JAR,
+            "-generateMOSuite",
+            "-Dalgorithm=DynaMOSA",
+            "-Dsearch_budget=180",
+            "-Dassertion_timeout=600",
+            "-Dminimization_timeout=600",
+            "-Dconfiguration_id=EvoSuite_fixed",
+            "-Doutput_variables=TARGET_CLASS,criterion,configuration_id,Total_Goals,Total_Branches,Lines,Covered_Goals,Generations,Statements_Executed,Fitness_Evaluations,Tests_Executed,Generations,Total_Time,Size,Result_Size,Length,Result_Length,BranchCoverage,Random_Seed",
+            "-Dshow_progress=FALSE",
+            "-mem","2500",
+            "-Dplot=false",
+            "-Dclient_on_thread=false",
+            "-class", clase_path
+            ]
+        # Nombre del archivo de salida
+        output_file = f'Dumps/seed_{iteracion}/DumpsInjected/' + clase + '_injected.txt'
+        output_directory = f'Dumps/seed_{iteracion}/DumpsInjected'
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        try:
+            # Ejecutar el comando y redirigir la salida al archivo
+            with open(output_file, 'w') as output:
+                subprocess.run(command, cwd="projects/" + proyecto, stdout=output, stderr=subprocess.PIPE, check=True)
+        except subprocess.CalledProcessError as e:
+            print("Error:", e)
+    print("Inyectado listo")
+
+#Copiar Tests
+for project in projects:
+    continue
+    source_folder = 'projects og/'+project+'/test_seeds'
+    destination_folder = 'tests_seeds/'+project
+    # Copy the folder and its contents
+    if os.path.exists(source_folder):
+        shutil.copytree(source_folder, destination_folder)
