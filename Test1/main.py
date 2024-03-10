3f1b3eed8dfe96b1ffedcc3da4682de29e871595
Nicolas Jimenez <NicoJmf@uc.cl>
2024-03-10 15:44:06 -0300

    Correr proyecto con 1 seed

--- Test1/main.py
+++ Test1/main.py
@@ -11,68 +11,73 @@ copiar_a_proyectos_inyectados()
 copiar_a_original()
 
 # Obtener un numero random de 13 digitos
-seed = str(random.randint(1000000000000, 9999999999999))
 #original
-for fila in info:
-    proyecto = fila[0]
-    clase_path = fila[1]
-    clase = fila[2]
-    print("Generando tests a la clase: ",clase)
-    # Comando sin redirección
-    command = [
-        'java', '-jar', 'evosuite-master-1.2.1-SNAPSHOT.jar',
-        '-class', clase_path,
-        '-projectCP', 'build/classes',
-        '-seed', seed,
-        '-Dshow_progress=FALSE',
-        '-Dsearch_budget=180',
-        '-Dassertion_timeout=600',
-        '-Dminimization_timeout=600',
-        '-Dconfiguration_id=EvoSuite_fixed'
-        '-Dalgorithm=DynaMOSA',
-    ]
-    # Nombre del archivo de salida
-    output_file = 'DumpsOriginal/' + clase + '_original.txt'
+for iteracion in range(1,2):
+    seed = str(random.randint(1000000000000, 9999999999999))
+    for fila in info:
+        proyecto = fila[0]
+        clase_path = fila[1]
+        clase = fila[2]
+        print("Generando tests a la clase: ",clase)
+        # Comando sin redirección
+        command = [
+            'java', '-jar', 'evosuite-master-1.2.1-SNAPSHOT.jar',
+            '-class', clase_path,
+            '-seed', seed,
+            '-Dshow_progress=FALSE',
+            '-Dsearch_budget=180',
+            '-Dassertion_timeout=600',
+            '-Dminimization_timeout=600',
+            '-Dconfiguration_id=EvoSuite_fixed'
+            '-Dalgorithm=DynaMOSA',
+            f'-Dtest_dir=test_seeds/seed_{iteracion}/tests_original'
+        ]
+        # Nombre del archivo de salida
+        output_file = f'Dumps/seed_{iteracion}/DumpsOriginal/' + clase + '_original.txt'
+        output_directory = f'Dumps/seed_{iteracion}/DumpsOriginal'
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
 
-    try:
-        # Ejecutar el comando y redirigir la salida al archivo
-        with open(output_file, 'w') as output:
-            subprocess.run(command, cwd="projects og/" + proyecto, stdout=output, stderr=subprocess.PIPE, check=True)
-    except subprocess.CalledProcessError as e:
-        print("Error:", e)
-print("Original listo")
+        try:
+            # Ejecutar el comando y redirigir la salida al archivo
+            with open(output_file, 'w') as output:
+                subprocess.run(command, cwd="projects og/" + proyecto, stdout=output, stderr=subprocess.PIPE, check=True)
+        except subprocess.CalledProcessError as e:
+            print("Error:", e)
+    print("Original listo")
 
 
+    # Injected
+    for fila in info:
+        clase_path = fila[1]
+        clase = fila[2]
+        proyecto = fila[0]
+        print("Generando tests a la clase: ",clase)
+        # Comando sin redirección
+        command = [
+            'java', '-jar', 'evosuite-master-1.2.1-SNAPSHOT.jar',
+            '-class', clase_path,
+            '-seed', seed,
+            '-Dshow_progress=FALSE',
+            '-Dsearch_budget=180',
+            '-Dassertion_timeout=600',
+            '-Dminimization_timeout=600',
+            '-Dconfiguration_id=EvoSuite_fixed',
+            '-Dalgorithm=DynaMOSA',
+            f'-Dtest_dir=test_seeds/seed_{iteracion}/tests_injected'
+        ]
+        # Nombre del archivo de salida
+        output_file = f'Dumps/seed_{iteracion}/DumpsInjected/' + clase + '_injected.txt'
+        output_directory = f'Dumps/seed_{iteracion}/DumpsInjected'
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
 
-# Injected
-for fila in info:
-    clase_path = fila[1]
-    clase = fila[2]
-    proyecto = fila[0]
-    print("Generando tests a la clase: ",clase)
-    # Comando sin redirección
-    command = [
-        'java', '-jar', 'evosuite-master-1.2.1-SNAPSHOT.jar',
-        '-class', clase_path,
-        '-projectCP', 'build/classes',
-        '-seed', seed,
-        '-Dshow_progress=FALSE',
-        '-Dsearch_budget=180',
-        '-Dassertion_timeout=600',
-        '-Dminimization_timeout=600',
-        '-Dconfiguration_id=EvoSuite_fixed',
-        '-Dalgorithm=DynaMOSA',
-    ]
-    # Nombre del archivo de salida
-    output_file = 'DumpsInjected/' + clase + '_injected.txt'
+        try:
+            # Ejecutar el comando y redirigir la salida al archivo
+            with open(output_file, 'w') as output:
+                subprocess.run(command, cwd="projects/" + proyecto, stdout=output, stderr=subprocess.PIPE, check=True)
+        except subprocess.CalledProcessError as e:
+            print("Error:", e)
 
-    try:
-        # Ejecutar el comando y redirigir la salida al archivo
-        with open(output_file, 'w') as output:
-            subprocess.run(command, cwd="projects/" + proyecto, stdout=output, stderr=subprocess.PIPE, check=True)
-    except subprocess.CalledProcessError as e:
-        print("Error:", e)
+    print("Inyectado listo")
 
-print("Inyectado listo")
-
-import get_results
