b198bdd2cc607c6daa8f732af37be75ca5b2135f
PapayaManco <nicojmf@uc.cl>
2024-03-15 10:21:00 -0300

    2 clases faltantes

--- Test1/Dumps/a.py
+++ Test1/Dumps/a.py
@@ -1,5 +1,3 @@
-#open every file in DumpsInjected and count if it has the string Writing JUnit
-
 import os
 
 def count_files_with_string(directory, string):
@@ -8,6 +6,8 @@ def count_files_with_string(directory, string):
         with open(directory + filename, 'r') as file:
             if string in file.read():
                 count += 1
+            else:
+                print(filename)
     return count
 
-print(count_files_with_string('seed_1/DumpsOriginal/', 'Writing JUnit'))
+print(count_files_with_string('seed_1/DumpsOriginal/', "Resulting test suite's mutation score"))
--- Test1/extraer_info.py
+++ Test1/extraer_info.py
@@ -1,7 +1,4 @@
-#open clases_info.csv
-
 import csv
-
 # Open the CSV file
 with open('clases_info.csv', newline='') as csvfile:
     # Create a CSV reader
@@ -9,7 +6,5 @@ with open('clases_info.csv', newline='') as csvfile:
 
     # Convert each row into a list
     rows_as_lists = [row for row in csv_reader]
-
-# Print the list of rows
 
 info = rows_as_lists
